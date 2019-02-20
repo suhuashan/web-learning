95dd43499711d640cebdd87ec3af9db062dca61b
jgchenu <jgchenu@foxmail.com>
2019-02-20 19:35:50 +0800

    增加了二栏 三栏布局，增加了垂直水平居中

---
+++ js/vue/hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  mounted() {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.hello {
+  font-size: 30px;
+  font-weight: 500;
+}
+</style>
---
+++ js/vue/hello-world/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" ref="hello" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    console.log(this.$refs.hello);
+    console.log(this.$refs.hello.$el);
+    console.log(this.$children[0] === this.$refs.hello);
+  }
+};
+</script>
